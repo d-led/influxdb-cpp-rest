1d6576a36a89cee7cf08a05e6650112485175bd6
Dmitry Ledentsov <dmlled@yahoo.com>
2016-12-09 20:56:14 +0100

    do not convert the lines to utf16

--- src/influxdb-cpp-rest/influxdb_raw_db.cpp
+++ src/influxdb-cpp-rest/influxdb_raw_db.cpp
@@ -45,6 +45,7 @@ string_t influxdb::raw::db::get(string_t const & query) {
     }
 }
 
+void influxdb::raw::db::insert(string_t const & db, std::string const & lines) {
     uri_builder builder(client.base_uri());
     builder.append(U("/write"));
     builder.append_query(U("db"), db);
