993d15279c3e7f71fa85b6741d4eb4fae3cab7a3
Dmitry Ledentsov <dmlled@yahoo.com>
2017-06-09 23:26:04 +0200

    #4 try testing on appveyor

---
+++ src/script/test.bat
@@ -0,0 +1,8 @@
+start influxd
+ping 127.0.0.1 -n 2 > nul
+%1\test -d yes
+taskkill /F /IM influxd.exe /T
+start influxd -config src\auth_test\influxdb.conf
+ping 127.0.0.1 -n 2 > nul
+%1\auth_test -d yes
+taskkill /F /IM influxd.exe /T
