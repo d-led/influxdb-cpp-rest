0ec7eff82cbb9240a3a3f2a62bfa440851d75643
Dmitry Ledentsov <dmlled@yahoo.com>
2019-05-02 23:38:18 +0200

    correct the test script

--- src/script/test.sh
+++ src/script/test.sh
@@ -2,17 +2,19 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+influxdb_version=1.7.6
+
 # first parameter to the test script (directory with the test binaries) is mandatory
 echo "running tests from $1"
 pkill influxd || true
-./influxdb-1.7.6-1/usr/bin/influxd &
+./influxdb-${influxdb_version}-1/usr/bin/influxd &
 sleep 3
 $1/test-influxdb-cpp-rest -d yes
 export LD_LIBRARY_PATH=$1
 $1/test-influxdb-c-rest -d yes
 pkill influxd
 sleep 3
-./influxdb-1.2.4-1/usr/bin/influxd -config src/auth_test/influxdb.conf &
+./influxdb-${influxdb_version}-1/usr/bin/influxd -config src/auth_test/influxdb.conf &
 sleep 3
 $1/test-influxdb-cpp-auth -d yes
 pkill influxd
